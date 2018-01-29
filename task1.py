42ae72cb4ebe7a97b04fb24f4e2c0c5a921e030e
sahilseth786 <Sahil.seth@dal.ca>
2018-01-29 15:29:13 -0400

    getting a string and returning a value

---
+++ task1.py
@@ -0,0 +1,10 @@
+#getting inut from user
+count = int (input("enter number of donuts "))
+def donuts(count):
+    if count<10:
+        print ("num of donuts is : {}".format(count))
+    else:
+        print ("num of donuts is : {}".format("Many"))
+donuts(count)
+
+
---
+++ task2.py
@@ -0,0 +1,8 @@
+def both_ends(s):
+    if len(s)<=2:
+        return ""
+    else:
+        a = s[0:2]+s[len(s)-2:]
+        return a
+
+print(both_ends("bannan"))
